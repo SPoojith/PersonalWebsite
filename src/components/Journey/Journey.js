d4f7c37151ebafd9f5afb7b42acf6f145588d959
SPoojith <spoojith15@gmail.com>
2024-09-01 02:45:15 +0530

    added journey video

--- src/components/Journey/Journey.js
+++ src/components/Journey/Journey.js
@@ -1,5 +1,6 @@
 import './Journey.css';
-import bgv from '../../Assests/c.mp4'
+// import bgv from '../../Assests/c.mp4'
+import bgv from '../../Assests/Journeyideo.mp4'
 // import bgv from '../../Assests/b.mov'
 
 function Journey() {
